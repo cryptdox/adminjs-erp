3945e188ec1b8968f0b201bb87b7de8dad45444e
Abir Hosen <abir71.hosen@gmail.com>
2025-07-10 17:50:32 +0600

    dashboard

--- src/admin/components/dashboard.tsx
+++ src/admin/components/dashboard.tsx
@@ -4,7 +4,7 @@ import { Box, H2, Text } from '@adminjs/design-system'
 const Dashboard = () => {
   return (
     <Box variant="grey">
-      <H2>Welcome to your custom AdminJS dashboard</H2>
+      <H2>Welcome to your ADMIN dashboard</H2>
       <Text>This is where you can add charts, stats, or anything you want.</Text>
     </Box>
   )
